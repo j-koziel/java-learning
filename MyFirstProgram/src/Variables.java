a50d9d2bfe80be059cbc446a6219362cae2f7023
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-02-02 19:50:10 +0000

    Variables

---
+++ MyFirstProgram/src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) throws Exception {
+        
+        byte x = 127;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Bro";
+        
+        System.out.println("Hello " + name);
+         
+    }
+}
