661f1830f34bcb3838e0fd764bf82eb733b3fd9b
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-02-02 20:03:34 +0000

    Switching variables

--- MyFirstProgram/src/Variables.java
+++ MyFirstProgram/src/Variables.java
@@ -1,13 +1,24 @@
 public class Variables {
     public static void main(String[] args) throws Exception {
         
-        byte x = 127;
-        double y = 3.14;
-        boolean z = true;
-        char symbol = '@';
-        String name = "Bro";
+        // The main data types which variables can have
+        byte x = 127; // bytes range from -128 - 127
+        double y = 3.14; // doubles can have 15 numbers after the dec. point
+        boolean z = true; // booleans are binary of course
+        char symbol = '@'; // char is a single character
+        String name = "Bro"; // sequence of chars
         
-        System.out.println("Hello " + name);
+        // Switching variables
+        String water = "water";
+        String koolAid = "Kool-Aid";
+        String temp; // temporary variable which will hold one of the values so that the other value can be put in instead
+
+        temp = water; // storing water in temp
+        water = koolAid; // putting koolaid into empty water
+        koolAid = temp; // koolaid being given temp (the water)
+
+        System.out.println("x: " + water);
+        System.out.println("y: " + koolAid);
          
     }
-}
+} 
