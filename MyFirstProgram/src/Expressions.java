b58b8c010a83ae160a5ff40b04a31863b71c8986
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-02-04 21:37:58 +0000

    Expressions

---
+++ MyFirstProgram/src/Expressions.java
@@ -0,0 +1,13 @@
+public class Expressions {
+  public static void main(String[] args) {
+    // Expressions are combinations of operands and operators
+    // operands = values, variables etc
+    // operators = + - * / %
+
+    double friends = 10;
+
+    friends = (double) friends / 3;
+
+    System.out.println(friends);
+  }
+}
