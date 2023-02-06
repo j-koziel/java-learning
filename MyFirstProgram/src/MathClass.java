c781a89fff378ba603fd99e42386dafb3dd5297e
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-02-06 22:01:10 +0000

    Generating random values

--- MyFirstProgram/src/MathClass.java
+++ MyFirstProgram/src/MathClass.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class MathClass {
   public static void main(String[] args) {
@@ -12,24 +13,31 @@ public class MathClass {
     double d = Math.floor(x); // round down
     double e = Math.ceil(x); // round up
 
-    // System.out.println(z);
+    // double f;
+    // double g;
+    // double h;
 
-    double f;
-    double g;
-    double h;
+    // Scanner scanner = new Scanner(System.in);
 
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.println("Enter side f: ");
-    f = scanner.nextDouble();
+    // System.out.println("Enter side f: ");
+    // f = scanner.nextDouble();
     
-    System.out.println("Enter side g: ");
-    g = scanner.nextDouble();
+    // System.out.println("Enter side g: ");
+    // g = scanner.nextDouble();
 
-    h = Math.sqrt((f * f) + (g * g));
+    // h = Math.sqrt((f * f) + (g * g));
 
-    System.out.println("The hypotenuse is: " + h);
+    // System.out.println("The hypotenuse is: " + h);
 
-    scanner.close();
+    // scanner.close();
+
+    // Random numbers
+    Random random = new Random(); // Not genuine random numbers, pretty close tho 🤷‍♂️
+
+    // int randomNum = random.nextInt(6) + 1;
+    // double randomDouble = random.nextDouble();
+    boolean randomBool = random.nextBoolean();
+
+    System.out.println(randomBool);
   }
 }
