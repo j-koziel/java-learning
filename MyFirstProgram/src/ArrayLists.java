333f9ea554b6753104259051a4318c3cd9004e06
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-02-16 20:00:18 +0000

    2D ArrayList

--- MyFirstProgram/src/ArrayLists.java
+++ MyFirstProgram/src/ArrayLists.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class ArrayLists {
   public static void main(String[] args) {
@@ -17,8 +17,35 @@ public class ArrayLists {
     food.remove(2);
     food.clear();
     
-    for (int i = 0; i < food.size(); i++) {
-      System.out.println(food.get(i));
-    }
+    // for (int i = 0; i < food.size(); i++) {
+    //   System.out.println(food.get(i));
+    // }
+
+    
+    // 2D ArrayList
+    // A list of lists
+    // The size of the lists can be changed during runtime
+    
+    ArrayList<ArrayList<String>> groceryList = new ArrayList();
+    
+    ArrayList<String> bakeryList = new ArrayList();
+    bakeryList.add("pasta");
+    bakeryList.add("garlic bread");
+    bakeryList.add("donuts");
+
+    ArrayList<String> produceList = new ArrayList();
+    produceList.add("tomatoes");
+    produceList.add("zuchinni");
+    produceList.add("peppers");
+
+    ArrayList<String> drinksList = new ArrayList();
+    drinksList.add("soda");
+    drinksList.add("coffee");
+
+    groceryList.add(bakeryList);
+    groceryList.add(produceList);
+    groceryList.add(drinksList);
+
+    System.out.println(groceryList.get(2).get(1));
   }
 }
