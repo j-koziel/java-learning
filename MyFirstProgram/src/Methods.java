326e4a7ef7bb7ff4d84ff04759aa6f1d7dd44055
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-02-24 19:18:00 +0000

    printf method

--- MyFirstProgram/src/Methods.java
+++ MyFirstProgram/src/Methods.java
@@ -4,11 +4,38 @@ public class Methods {
     String name = "Jonathan";
     int age = 21;
 
-    hello(name, age);
+    // hello(name, age);
 
-    double x = add(1.0, 2.0, 3.0, 4.0);
+    // double x = add(1.0, 2.0, 3.0, 4.0);
 
-    System.out.println(x);
+    // System.out.println(x);
+
+    // printf method -> an optional method to control, format and display text
+    // two arguments -> format string + some value you want in the string
+    boolean myBoolean = true;
+    char myChar = '@';
+    String myString = "Jonathan";
+    int myInt = 50;
+    double myDouble = 1000;
+
+    // conversion characters to match certain data types
+    // System.out.printf("%b", myBoolean);
+    // System.out.printf("%c", myChar);
+    // System.out.printf("%s", myString);
+    // System.out.printf("%d", myInt);
+    // System.out.printf("%f", myDouble);
+
+    // width
+    // minimum number of chars to be written as output
+    // System.out.printf("hello %10s", myString);
+
+    // precision
+    // sets number of digits of precision when outputting decimals 
+    // System.out.printf("You have this much money %.2f", myDouble);
+
+    // flags
+    // adds an effect to the output based on the flag
+    System.out.printf("You have this much money %,f", myDouble);
   }
 
   // type of method -> return type -> method def
