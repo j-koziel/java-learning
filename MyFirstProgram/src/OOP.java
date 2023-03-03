e10fa44904b25134acd6ebb2725f7f757ef4289c
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-03-03 22:56:33 +0000

    Constructors (BroCode Java)

--- MyFirstProgram/src/OOP.java
+++ MyFirstProgram/src/OOP.java
@@ -1,12 +1,22 @@
 public class OOP {
   public static void main(String[] args) {
     // object = an instance of a class which may contain attributes or methods
-    Car myCar = new Car();
+//    Car myCar = new Car();
 
-    System.out.println(myCar.make);
-    System.out.println(myCar.model);
-    myCar.drive();
-    myCar.brake();
+//    System.out.println(myCar.make);
+//    System.out.println(myCar.model);
+//    myCar.drive();
+//    myCar.brake();
+
+    Human human1 = new Human("Rick", 65, 70);
+    Human human2 = new Human("Morty", 16, 50);
+    System.out.println(human1.name);
+    System.out.println(human1.age);
+    System.out.println(human1.weight);
+
+    System.out.println(human2.name);
+    System.out.println(human2.age);
+    System.out.println(human2.weight);
   }
 }
 
@@ -24,5 +34,16 @@ class Car {
   
   void brake() {
     System.out.println("You step on the brakes");
+  }
+}
+
+class Human {
+  String name;
+  int age;
+  double weight;
+  Human(String name, int age, double weight) {
+    this.name = name;
+    this.age = age;
+    this.weight = weight;
   }
 }
