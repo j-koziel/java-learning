789e679eaa2d93496d6e211e42d953412fa9c731
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-03-04 18:54:17 +0000

    Business layer (Spring boot)

--- MyFirstProgram/src/OOP.java
+++ MyFirstProgram/src/OOP.java
@@ -10,13 +10,9 @@ public class OOP {
 
     Human human1 = new Human("Rick", 65, 70);
     Human human2 = new Human("Morty", 16, 50);
-    System.out.println(human1.name);
-    System.out.println(human1.age);
-    System.out.println(human1.weight);
 
-    System.out.println(human2.name);
-    System.out.println(human2.age);
-    System.out.println(human2.weight);
+    human2.eat();
+    human1.drink();
   }
 }
 
@@ -46,4 +42,13 @@ class Human {
     this.age = age;
     this.weight = weight;
   }
+
+  void eat() {
+    System.out.println(this.name + " is eating");
+  }
+
+  void drink() {
+    System.out.println(this.name + " is drinking");
+  }
+
 }
--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -10,13 +10,14 @@ import java.time.Month;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
-class StudentController {
+public class StudentController {
+  private final StudentService studentService;
+
+  public StudentController(StudentService studentService) {
+    this.studentService = studentService;
+  }
   @GetMapping("")
   public List<Student> getStudents() {
-    return List.of(new Student(1L,
-        "Mariam",
-        "mariamjamal@gmail.com",
-        LocalDate.of(2000, Month.JANUARY, 5),
-        21));
+    return studentService.getStudents();
   }
 }
