a525a2ba71ad43df947bfec13306182ed7968d87
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-03-19 21:33:08 +0000

    writing some business logic (spring boot)

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -25,7 +26,13 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> studentOptional = studentRepository.findStudentsByEmail(student.getEmail());
+
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+
+        studentRepository.save(student);
 
     }
 }
