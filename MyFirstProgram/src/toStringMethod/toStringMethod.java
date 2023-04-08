910ff6925209b8a70388c989ffaa7761f667585f
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-04-08 21:49:17 +0100

    toString method (bro code)

---
+++ MyFirstProgram/src/toStringMethod/toStringMethod.java
@@ -0,0 +1,13 @@
+package toStringMethod;
+
+public class toStringMethod {
+    public static void main(String[] args) {
+        // this is a special method which returns the values of a class instance as a string
+        Car car = new Car();
+
+//        System.out.println(car.toString());
+        System.out.println(car);
+
+    }
+
+}
