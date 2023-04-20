182db55a49f03d76789e70b3c8c7172ba64116e1
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-04-20 22:01:58 +0100

    arrays of objects (brocode)

---
+++ MyFirstProgram/src/arrayOfObjects/Main.java
@@ -0,0 +1,20 @@
+package arrayOfObjects;
+
+public class Main {
+    public static void main(String[] args) {
+//        Food[] refrigerator = new Food[3];
+
+        Food food1 = new Food("pizza");
+        Food food2 = new Food("hamburger");
+        Food food3 = new Food("hotdog");
+
+        Food[] refrigerator = {food1, food2, food3};
+
+        System.out.println(refrigerator[0].name);
+        System.out.println(refrigerator[1].name);
+        System.out.println(refrigerator[2].name);
+
+
+    }
+
+}
