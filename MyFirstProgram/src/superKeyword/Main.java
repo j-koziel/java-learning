3a3ad584551aa38f4d8ecd62d1c353a39bf4080f
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-06-07 12:32:29 +0100

    The super keyword

---
+++ MyFirstProgram/src/superKeyword/Main.java
@@ -0,0 +1,13 @@
+package superKeyword;
+
+public class Main {
+    public static void main(String[] args) {
+    //    super is a keyword which refers to the parent class or superclass of an object
+    //    The super keyword allows you to call the constructor of the parent class without having to assign the same
+    //    fields and values again.
+        Hero hero1 = new Hero("Batman", 42, "$$$");
+        Hero hero2 = new Hero("Superman", 43, "everything");
+
+        System.out.println(hero2.toString());
+    }
+}
