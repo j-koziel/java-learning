16de4b6782ba4fefb8dc28a76637275e84f66e10
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-06-07 12:33:17 +0100

    More notes on the super keyword

--- MyFirstProgram/src/superKeyword/Main.java
+++ MyFirstProgram/src/superKeyword/Main.java
@@ -5,6 +5,8 @@ public class Main {
     //    super is a keyword which refers to the parent class or superclass of an object
     //    The super keyword allows you to call the constructor of the parent class without having to assign the same
     //    fields and values again.
+    //    It also allows you to call methods of the parent class which gives more flexibility in specific implementation
+    //    of methods.
         Hero hero1 = new Hero("Batman", 42, "$$$");
         Hero hero2 = new Hero("Superman", 43, "everything");
 
