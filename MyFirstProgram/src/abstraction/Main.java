089baf5fd323b2cd1f195c0da1af73f64f070843
Jonathan Koziel <jonathankoziel1@gmail.com>
2023-06-07 12:45:20 +0100

    The abstract keyword

---
+++ MyFirstProgram/src/abstraction/Main.java
@@ -0,0 +1,16 @@
+package abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+    //    The abstract keyword can be used with classes and methods
+    //    Abstract classes cannot be instantiated, but they can have child classes
+    //    It adds a layer of security to your code
+    //    Abstract methods are declared but without an implementation
+    //    They are just a general idea of what the child classes should look like
+
+        // Vehicle vehicle = new Vehicle();
+        Car car = new Car();
+
+        car.go();
+    }
+}
